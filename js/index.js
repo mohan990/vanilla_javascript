0db1d8af5d905c5c26264a6a662faa5409cb63bb
mohan990 <66485910+mohan990@users.noreply.github.com>
2021-09-19 22:18:14 +0530

    mock api

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+var input = document.querySelector("#txt-input")
+var output = document.querySelector(".output-text")
+var btnTranslate = document.querySelector("#btn-translate")
+
+var FunTransUrl = "https://api.funtranslations.com/translate/minion.json"
+ 
+
+
+
+
+
+
+
+// function clickEvent()
+// {
+//     output.innerHTML= input.value;
+// }
+
+// btnTranslate.addEventListener("click", clickEvent)
